8911767ef10f1374382bb7d355ea59b53a4a3246
be happy <sky19352593@gmail.com>
2025-05-24 17:15:34 +0800

    Fix swagger page error

--- happy-admin/src/main/java/org/happy/HappyApplication.java
+++ happy-admin/src/main/java/org/happy/HappyApplication.java
@@ -14,15 +14,16 @@ public class HappyApplication {
     public static void main(String[] args) {
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(HappyApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  开心启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                           " .-------.       ____     __        \n" +
-                           " |  _ _   \\      \\   \\   /  /    \n" +
-                           " | ( ' )  |       \\  _. /  '       \n" +
-                           " |(_ o _) /        _( )_ .'         \n" +
-                           " | (_,_).' __  ___(_ o _)'          \n" +
-                           " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                           " |  | \\ `'   /|   `-'  /           \n" +
-                           " |  |  \\    /  \\      /           \n" +
-                           " ''-'   `'-'    `-..-'              ");
+        System.out.println("""
+                (♥◠‿◠)ﾉﾞ  开心启动成功   ლ(´ڡ`ლ)ﾞ \s
+                 .-------.       ____     __       \s
+                 |  _ _   \\      \\   \\   /  /   \s
+                 | ( ' )  |       \\  _. /  '      \s
+                 |(_ o _) /        _( )_ .'        \s
+                 | (_,_).' __  ___(_ o _)'         \s
+                 |  |\\ \\  |  ||   |(_,_)'        \s
+                 |  | \\ `'   /|   `-'  /          \s
+                 |  |  \\    /  \\      /          \s
+                 ''-'   `'-'    `-..-'             \s""");
     }
 }
